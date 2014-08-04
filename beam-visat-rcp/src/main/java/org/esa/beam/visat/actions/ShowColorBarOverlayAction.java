fbed21c682b9db0e452e03bf16025df996febd95
Aynur Abdurazik <aynur.abdurazik@nasa.gov>
2014-08-04 15:59:08 -0400

    Fixed a minor bug with color bar scaling with respect to the image size.

--- beam-visat-rcp/src/main/java/org/esa/beam/visat/actions/ShowColorBarOverlayAction.java
+++ beam-visat-rcp/src/main/java/org/esa/beam/visat/actions/ShowColorBarOverlayAction.java
@@ -6,11 +6,22 @@ import com.bc.ceres.glayer.LayerFilter;
 import com.bc.ceres.glayer.LayerType;
 import com.bc.ceres.glayer.LayerTypeRegistry;
 import com.bc.ceres.glayer.support.LayerUtils;
-import org.esa.beam.framework.datamodel.RasterDataNode;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import org.esa.beam.framework.datamodel.*;
 import org.esa.beam.framework.ui.command.CommandEvent;
 import org.esa.beam.framework.ui.product.ProductSceneView;
 import org.esa.beam.glayer.ColorBarLayerType;
+import org.esa.beam.util.FeatureUtils;
 import org.esa.beam.visat.VisatApp;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.geotools.geometry.jts.GeometryCoordinateSequenceTransformer;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.TransformException;
 
 import javax.media.jai.TiledImage;
 import java.awt.geom.AffineTransform;
@@ -28,6 +39,7 @@ public class ShowColorBarOverlayAction extends AbstractShowOverlayAction {
     private static final String COLORBAR_TYPE_PROPERTY_NAME = "colorbar.type";
     private static final String DEFAULT_LAYER_TYPE = "ColorBarLayerType";
     private RenderedImage colorBarImage;
+    private FeatureCollection<SimpleFeatureType, SimpleFeature> featureCollection;
 
     @Override
     public void actionPerformed(CommandEvent event) {
@@ -58,7 +70,33 @@ public class ShowColorBarOverlayAction extends AbstractShowOverlayAction {
         return layerType.createLayer(null, template);
     }
 
+    public void createColorBarVectorNode(){
+        VisatApp visatApp = VisatApp.getApp();
+        final ProductSceneView sceneView = VisatApp.getApp().getSelectedProductSceneView();
+        Product product = visatApp.getSelectedProduct();
+        String vectorName = "Color Bar";
 
+        final CoordinateReferenceSystem mapCRS = product.getGeoCoding().getMapCRS();
+        if (mapCRS.equals(DefaultGeographicCRS.WGS84)) {
+            try {
+                transformFeatureCollection(featureCollection, product.getGeoCoding().getImageCRS(), mapCRS);
+            } catch (TransformException e) {
+                VisatApp.getApp().showErrorDialog("transformation failed!");
+            }
+        }
+        final PlacemarkDescriptor placemarkDescriptor = PlacemarkDescriptorRegistry.getInstance().getPlacemarkDescriptor(featureCollection.getSchema());
+        placemarkDescriptor.setUserDataOf(featureCollection.getSchema());
+        VectorDataNode vectorDataNode = new VectorDataNode(vectorName, featureCollection, placemarkDescriptor);
+//        String hex = String.format("#%02x%02x%02x", Color.red, Color.green, Color.blue);
+//        vectorDataNode.setDefaultStyleCss(String.format( "fill:%s; fill-opacity:0.5; stroke:%s; stroke-opacity:1.0; stroke-width:1.0; symbol:plus", hex, hex));
+        if (product.getVectorDataGroup().contains(vectorDataNode.getName())) {
+            product.getVectorDataGroup().remove(product.getVectorDataGroup().get(vectorDataNode.getName()));
+        }
+        product.getVectorDataGroup().add(vectorDataNode);
+        if (sceneView != null) {
+            sceneView.setLayersVisible(vectorDataNode);
+        }
+    }
     @Override
     protected void updateEnableState(ProductSceneView view) {
         setEnabled(true);
@@ -101,6 +139,7 @@ public class ShowColorBarOverlayAction extends AbstractShowOverlayAction {
         AffineTransform transform = raster.getSourceImage().getModel().getImageToModelTransform(0);
         transform.concatenate(createTransform(raster, image));
         return transform;
+        //return createTransform(raster, image);
     }
 
     private AffineTransform createTransform(RasterDataNode raster, RenderedImage colorBarImage) {
@@ -114,8 +153,11 @@ public class ShowColorBarOverlayAction extends AbstractShowOverlayAction {
         double scaleX = (colorBarImageHeight < colorBarImageWidth) ? (double) rasterWidth / colorBarImageWidth : 0.6;
         //if color bar is vertical
         double scaleY = (colorBarImageHeight > colorBarImageWidth) ? (double) rasterHeight / colorBarImageHeight : 0.6;
-        if (scaleX > 1) scaleY = scaleY + 1;
-        if (scaleY > 1) scaleX = scaleX + 1;
+        if (scaleX > 1) {
+            scaleY = scaleY + 1;
+        }   else if (scaleY > 1) {
+            scaleX = scaleX + 1;
+        }
         int y_axis_translation = (colorBarImageHeight < colorBarImageWidth) ? rasterHeight - colorBarImageHeight / 4 : 0;
         int x_axis_translation = (colorBarImageHeight < colorBarImageWidth) ? 0 : rasterWidth - colorBarImageWidth / 10;
         double[] flatmatrix = {scaleX, 0.0, 0.0, scaleY, x_axis_translation, y_axis_translation};
@@ -123,4 +165,23 @@ public class ShowColorBarOverlayAction extends AbstractShowOverlayAction {
         return i2mTransform;
     }
 
+    private static void transformFeatureCollection(FeatureCollection<SimpleFeatureType, SimpleFeature> featureCollection, CoordinateReferenceSystem sourceCRS, CoordinateReferenceSystem targetCRS) throws TransformException {
+        final GeometryCoordinateSequenceTransformer transform = FeatureUtils.getTransform(sourceCRS, targetCRS);
+        final FeatureIterator<SimpleFeature> features = featureCollection.features();
+        final GeometryFactory geometryFactory = new GeometryFactory();
+        while (features.hasNext()) {
+            final SimpleFeature simpleFeature = features.next();
+            final LineString sourceLine = (LineString) simpleFeature.getDefaultGeometry();
+            final LineString targetLine = transform.transformLineString(sourceLine, geometryFactory);
+            simpleFeature.setDefaultGeometry(targetLine);
+        }
+    }
+
+    public FeatureCollection<SimpleFeatureType, SimpleFeature> getFeatureCollection() {
+        return featureCollection;
+    }
+
+    public void setFeatureCollection(FeatureCollection<SimpleFeatureType, SimpleFeature> featureCollection) {
+        this.featureCollection = featureCollection;
+    }
 }
