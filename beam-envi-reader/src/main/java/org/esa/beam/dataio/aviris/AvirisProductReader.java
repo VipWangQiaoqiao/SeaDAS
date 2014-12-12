ef9ebb6ada14f548e2630f41cf72e3a765b69296
Sean Bailey <sean.w.bailey@nasa.gov>
2014-12-12 11:01:32 -0500

    modifications to ENVI reader and the AVIRIS "sub-module" to geolocate AVIRIS properly

--- beam-envi-reader/src/main/java/org/esa/beam/dataio/aviris/AvirisProductReader.java
+++ beam-envi-reader/src/main/java/org/esa/beam/dataio/aviris/AvirisProductReader.java
@@ -1,6 +1,9 @@
 package org.esa.beam.dataio.aviris;
 
 import com.bc.ceres.core.ProgressMonitor;
+import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
+import java.io.BufferedReader;
 import org.esa.beam.dataio.envi.EnviProductReaderPlugIn;
 import org.esa.beam.framework.dataio.AbstractProductReader;
 import org.esa.beam.framework.dataio.ProductIOException;
@@ -10,13 +13,27 @@ import org.esa.beam.framework.datamodel.*;
 import org.esa.beam.util.ProductUtils;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.EnumMap;
+import org.esa.beam.dataio.envi.EnviConstants;
+import org.esa.beam.dataio.envi.EnviCrsFactory;
+import org.esa.beam.dataio.envi.EnviMapInfo;
+import org.esa.beam.dataio.envi.EnviProjectionInfo;
+import org.esa.beam.dataio.envi.Header;
+import org.esa.beam.util.Debug;
+import org.geotools.referencing.ReferencingFactoryFinder;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CRSAuthorityFactory;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.TransformException;
 
 class AvirisProductReader extends AbstractProductReader {
+    private Header header;
 //   *eph                  position data in a WGS-84/NAD83 UTM x,y,z coordinate system,
 //   *lonlat_eph           position in WGS-84 longitude, latitude and elevation,
 //   *obs                  parameters relating to the geometry of the observation and
@@ -82,31 +99,39 @@ class AvirisProductReader extends AbstractProductReader {
         int sceneHeight = 0;
         AvirisFilename genericAvirisFilename = null;
         for (File hdrFile : hdrFiles) {
-            ProductReader enviProductReader = enviProductReaderPlugIn.createReaderInstance();
-            Product product = enviProductReader.readProductNodes(hdrFile, null);
-            if (sceneHeight == 0) {
-                sceneHeight = product.getSceneRasterHeight();
-                sceneWidth = product.getSceneRasterWidth();
-            }
+            
             AvirisFilename avirisFilename = AvirisFilename.create(hdrFile.getName());
             if (genericAvirisFilename == null) {
                 genericAvirisFilename = avirisFilename;
             }
             FileType fileType = FileType.fromString(avirisFilename.getFileType());
-            avirisProductParts.put(fileType, product);
+            if (fileType == FileType.RAD || fileType == FileType.ORT){
+                ProductReader enviProductReader = enviProductReaderPlugIn.createReaderInstance();
+                Product product = enviProductReader.readProductNodes(hdrFile, null);
+                if (sceneHeight == 0) {
+                    sceneHeight = product.getSceneRasterHeight();
+                    sceneWidth = product.getSceneRasterWidth();
+                }
+                avirisProductParts.put(fileType, product);
+                if (fileType == FileType.RAD){
+                    final BufferedReader headerReader =  new BufferedReader(new FileReader(hdrFile));
+                    header = new Header(headerReader);
+                }
+            }
         }
         Product product = new Product(genericAvirisFilename.getProductBase(), genericAvirisFilename.getFileType(),
                                       sceneWidth, sceneHeight);
         product.setDescription("AVIRIS data product");
         handleRadianceProduct(product);
-        handleGeomProduct(product);
+//        handleGeomProduct(product);
         handleObsOrtProduct(product);
+        initGeoCoding(product);
         return product;
     }
 
     private void handleRadianceProduct(Product product) {
         Product avirisProductPart = avirisProductParts.get(FileType.RAD);
-        if (avirisProductPart != null) {
+        if (avirisProductPart != null) {            
             copyMetadata(avirisProductPart, product, FileType.RAD.toString());
             String[] bandNames = avirisProductPart.getBandNames();
             for (String bandName : bandNames) {
@@ -120,6 +145,60 @@ class AvirisProductReader extends AbstractProductReader {
             product.setFileLocation(avirisProductPart.getFileLocation());
         }
     }
+    
+    private void initGeoCoding(final Product product) {
+        final EnviMapInfo enviMapInfo = header.getMapInfo();
+        if (enviMapInfo == null) {
+            return;
+        }
+        final EnviProjectionInfo projectionInfo = header.getProjectionInfo();
+        CoordinateReferenceSystem crs = null;
+        if (projectionInfo != null) {
+            try {
+                crs = EnviCrsFactory.createCrs(projectionInfo.getProjectionNumber(), projectionInfo.getParameter(),
+                                               enviMapInfo.getDatum(), enviMapInfo.getUnit());
+            } catch (IllegalArgumentException ignore) {
+            }
+        }
+        if (EnviConstants.PROJECTION_NAME_WGS84.equalsIgnoreCase(enviMapInfo.getProjectionName())) {
+            crs = DefaultGeographicCRS.WGS84;
+        } else if ("UTM".equalsIgnoreCase(enviMapInfo.getProjectionName())) {
+            try {
+                final int zone = enviMapInfo.getUtmZone();
+                final String hemisphere = enviMapInfo.getUtmHemisphere();
+
+                if (zone >= 1 && zone <= 60) {
+                    final CRSAuthorityFactory factory = ReferencingFactoryFinder.getCRSAuthorityFactory("EPSG", null);
+                    if ("North".equalsIgnoreCase(hemisphere)) {
+                        final int WGS84_UTM_zone_N_BASE = 32600;
+                        crs = factory.createProjectedCRS("EPSG:" + (WGS84_UTM_zone_N_BASE + zone));
+                    } else {
+                        final int WGS84_UTM_zone_S_BASE = 32700;
+                        crs = factory.createProjectedCRS("EPSG:" + (WGS84_UTM_zone_S_BASE + zone));
+                    }
+                }
+            } catch (FactoryException ignore) {
+            }
+        }
+
+
+        if (crs != null) {
+            try {
+                AffineTransform i2m = new AffineTransform();
+                i2m.translate(enviMapInfo.getEasting(), enviMapInfo.getNorthing());
+                i2m.scale(enviMapInfo.getPixelSizeX(), -enviMapInfo.getPixelSizeY());
+                i2m.rotate(Math.toRadians(-enviMapInfo.getOrientation()));
+                i2m.translate(-(enviMapInfo.getReferencePixelX() - 1), -(enviMapInfo.getReferencePixelY() - 1));
+                Rectangle rect = new Rectangle(product.getSceneRasterWidth(), product.getSceneRasterHeight());
+                GeoCoding geoCoding = new CrsGeoCoding(crs, rect, i2m);
+
+                product.setGeoCoding(geoCoding);
+            } catch (FactoryException | TransformException fe) {
+                Debug.trace(fe);
+            }
+        }
+
+    }
 
     private void handleGeomProduct(Product product) throws IOException {
         Product avirisProductPart = avirisProductParts.get(FileType.GEOM);
@@ -132,25 +211,28 @@ class AvirisProductReader extends AbstractProductReader {
             for (String bandName : bandNames) {
                 if (bandName.startsWith("Band_2")) {
                     latitudeBand = avirisProductPart.getBand(bandName);
+                    ProductUtils.copyBand(bandName, avirisProductPart, "latitude", product, true);
                 } else if (bandName.startsWith("Band_1")) {
                     longitudeBand = avirisProductPart.getBand(bandName);
+                    ProductUtils.copyBand(bandName, avirisProductPart, "longitude", product, true);
                 } else {
-                    String newBandname = "elevation";
-                    ProductUtils.copyBand(bandName, avirisProductPart, newBandname, product, true);
+                    ProductUtils.copyBand(bandName, avirisProductPart, "elevation", product, true);
                 }
             }
             if (latitudeBand != null && longitudeBand != null) {
                 // convert bands into tie-points
                 // to create a tie-point geo-coding, because it is much faster than a pixel-geo-coding
+//                int rasterWidth = latitudeBand.getSceneRasterWidth();
+//                int rasterHeight = latitudeBand.getSceneRasterHeight();
 //                float[] latData = latitudeBand.readPixels(0, 0, rasterWidth, rasterHeight, (float[]) null);
 //                TiePointGrid tpLat = new TiePointGrid("latitude", rasterWidth, rasterHeight, 0.5f, 0.5f, 1f, 1f, latData);
 //                product.addTiePointGrid(tpLat);
-
+//
 //                float[] lonData = longitudeBand.readPixels(0, 0, rasterWidth, rasterHeight, (float[]) null);
 //                TiePointGrid tpLon = new TiePointGrid("longitude", rasterWidth, rasterHeight, 0.5f, 0.5f, 1f, 1f, lonData);
 //                product.addTiePointGrid(tpLon);
 //                product.setGeoCoding(new TiePointGeoCoding(tpLat, tpLon));
-                product.setGeoCoding(new PixelGeoCoding(latitudeBand, longitudeBand,null,4));
+//                product.setGeoCoding(new PixelGeoCoding(latitudeBand, longitudeBand,null,4));
             }
         }
     }
