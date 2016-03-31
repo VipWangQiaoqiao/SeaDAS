912908838162311c8a3b0a4b7cf22c145fe418cd
Danny Knowles <daniel.s.knowles@nasa.gov>
2016-03-31 15:45:08 -0400

    Additional web links to go under the help menu.  Right now they are hard coded addresses.

---
+++ beam-visat-rcp/src/main/java/org/esa/beam/visat/actions/ShowForumPage.java
@@ -0,0 +1,43 @@
+package org.esa.beam.visat.actions;
+
+import org.esa.beam.framework.ui.command.CommandEvent;
+import org.esa.beam.framework.ui.command.ExecCommand;
+import org.esa.beam.util.SystemUtils;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: knowles
+ * Date: 3/31/16
+ * Time: 2:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShowForumPage extends ExecCommand {
+    @Override
+    public void updateState(final CommandEvent event) {
+        setEnabled(Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE));
+    }
+
+    /**
+     * Launches the default browser to display the BEAM Wiki.
+     * Invoked when a command action is performed.
+     *
+     * @param event the command event.
+     */
+    @Override
+    public void actionPerformed(CommandEvent event) {
+        final String homePageUrl = SystemUtils.getApplicationHomepageUrl();
+
+        try {
+            final Desktop desktop = Desktop.getDesktop();
+            desktop.browse(URI.create("http://oceancolor.gsfc.nasa.gov/forum/oceancolor/forum_show.pl"));
+        } catch (IOException e) {
+            // TODO - handle
+        } catch (UnsupportedOperationException e) {
+            // TODO - handle
+        }
+    }
+}
---
+++ beam-visat-rcp/src/main/java/org/esa/beam/visat/actions/ShowTutorialPageAction.java
@@ -0,0 +1,44 @@
+package org.esa.beam.visat.actions;
+
+import org.esa.beam.framework.ui.command.CommandEvent;
+import org.esa.beam.framework.ui.command.ExecCommand;
+import org.esa.beam.util.SystemUtils;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: knowles
+ * Date: 3/31/16
+ * Time: 1:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShowTutorialPageAction  extends ExecCommand {
+
+    @Override
+    public void updateState(final CommandEvent event) {
+        setEnabled(Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE));
+    }
+
+    /**
+     * Launches the default browser to display the BEAM Wiki.
+     * Invoked when a command action is performed.
+     *
+     * @param event the command event.
+     */
+    @Override
+    public void actionPerformed(CommandEvent event) {
+        final String homePageUrl = SystemUtils.getApplicationHomepageUrl();
+
+        try {
+            final Desktop desktop = Desktop.getDesktop();
+            desktop.browse(URI.create(homePageUrl+"/tutorial"));
+        } catch (IOException e) {
+            // TODO - handle
+        } catch (UnsupportedOperationException e) {
+            // TODO - handle
+        }
+    }
+}
