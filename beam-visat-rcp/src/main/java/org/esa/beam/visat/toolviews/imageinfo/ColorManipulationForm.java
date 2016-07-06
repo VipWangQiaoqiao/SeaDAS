3d1c3cbee5f0b2c0fd4ba91044abc78164d2baf8
Danny Knowles <daniel.s.knowles@nasa.gov>
2016-07-06 17:58:26 -0400

    L2 masks is now built off of a configuration file.  This file l2_masks_config.txt is located in .seadas/beam-ui/masks.  This enables users to customize masks which get built by default when loading a level-2 file.   NOTE: the code for installing this config file occurs and is located in ColorManipulationForm with all the other current beam-ui/auxdata installers (it was convenient to do this :).  We may want to move this code at some point.

--- beam-visat-rcp/src/main/java/org/esa/beam/visat/toolviews/imageinfo/ColorManipulationForm.java
+++ beam-visat-rcp/src/main/java/org/esa/beam/visat/toolviews/imageinfo/ColorManipulationForm.java
@@ -84,6 +84,7 @@ class ColorManipulationForm {
     private final ProductNodeListener productNodeListener;
     private boolean defaultColorPalettesInstalled;
     private boolean defaultRgbProfilesInstalled;
+    private boolean defaultMasksInstalled;
     private JPanel contentPanel;
     private final ColorManipulationToolView toolView;
     private ColorManipulationChildForm childForm;
@@ -125,6 +126,11 @@ class ColorManipulationForm {
         if (!defaultRgbProfilesInstalled) {
             installDefaultRgbProfiles();
         }
+
+        if (!defaultMasksInstalled) {
+            installDefaultMaskProfiles();
+        }
+
         return contentPanel;
     }
 
@@ -973,6 +979,52 @@ class ColorManipulationForm {
         };
         swingWorker.executeWithBlocking();
     }
+
+
+    // todo Danny added this masks installer.  It may be odd to have it here but this is where all auxdir installers currently are and was easy to add here.  Later it can be relocated if desired
+    private File getMasksAuxdataDir() {
+        return new File(SystemUtils.getApplicationDataDir(), "beam-ui/auxdata/masks");
+    }
+
+    private void installDefaultMaskProfiles() {
+        final URL codeSourceUrl = BeamUiActivator.class.getProtectionDomain().getCodeSource().getLocation();
+        final File auxdataDir = getMasksAuxdataDir();
+
+        final ResourceInstaller resourceInstaller = new ResourceInstaller(codeSourceUrl, "auxdata/masks/",
+                auxdataDir);
+        ProgressMonitorSwingWorker swingWorker = new ProgressMonitorSwingWorker(toolView.getPaneControl(),
+                "Installing Masks Auxdata...") {
+            @Override
+            protected Object doInBackground(ProgressMonitor progressMonitor) throws Exception {
+                resourceInstaller.install(".*.txt", progressMonitor, false);
+                defaultMasksInstalled = true;
+                return Boolean.TRUE;
+            }
+
+            /**
+             * Executed on the <i>Event Dispatch Thread</i> after the {@code doInBackground}
+             * method is finished. The default
+             * implementation does nothing. Subclasses may override this method to
+             * perform completion actions on the <i>Event Dispatch Thread</i>. Note
+             * that you can query status inside the implementation of this method to
+             * determine the result of this task or whether this task has been cancelled.
+             *
+             * @see #doInBackground
+             * @see #isCancelled()
+             * @see #get
+             */
+            @Override
+            protected void done() {
+                try {
+                    get();
+                } catch (Exception e) {
+                    visatApp.getLogger().log(Level.SEVERE, "Could not install Masks auxdata", e);
+                }
+            }
+        };
+        swingWorker.executeWithBlocking();
+    }
+
 
 
     private ImageInfo createDefaultImageInfo() {
